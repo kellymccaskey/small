1cda765bbbc1fc1cfbc934a3f5a770686bfa6aa9
carlislerainey <carlislerainey@gmail.com>
2015-03-30 06:31:35 -0400

    add coefficient plot

--- weisiger-replication/R/weisiger-replication.R
+++ weisiger-replication/R/weisiger-replication.R
@@ -6,11 +6,11 @@ d <- read.csv("data/conq_ins_data.tab", sep = "\t")
 
 # load packages
 library(logistf)
-library(coda)
 library(texreg)
 library(MASS)
 library(compactr)
 library(boot)
+library(arm)
 
 # create variables
 d$terrain_alt <- d$terrain/100
@@ -30,9 +30,8 @@ firth <- function(f, data, indices) {
   m <- logistf(f, data)
   return(m$coef) 
 } 
-
-# bootstrap
-bootstrap <- boot(d, statistic = firth, R = 1000, f = f)
+bootstrap <- boot(d, statistic = firth, R = 10000, f = f,
+                  parallel = "multicore", ncpus = 4)
 
 # calculate and compare first differences
 # set values to calculate first differences
@@ -45,7 +44,6 @@ x.gdppc2_alt <- median(d$gdppc2_alt, na.rm = TRUE)
 x.coord.hi <- quantile(d$coord, 1, na.rm = TRUE)
 x.coord.lo <- quantile(d$coord, 0, na.rm = TRUE)
 
-
 # calculating first differences
 # creating beta.hats; X.lo, X.hi, and X.c matrices; Sigmas; and beta.tildes
 beta.hat.lpm <- coef(lpm)
@@ -57,13 +55,13 @@ X.lo <- c(1, x.polity_conq.dem, x.lndist, x.terrain_alt, x.soldperterr, x.gdppc2
 X.c <- rbind(X.hi, X.lo)
 
 Sigma.lpm <- vcov(lpm)
-beta.tilde.lpm <- mvrnorm(1000, beta.hat.lpm, Sigma.lpm)
+beta.tilde.lpm <- mvrnorm(10000, beta.hat.lpm, Sigma.lpm)
 
 Sigma.logit <- vcov(logit)
-beta.tilde.logit <- mvrnorm(1000, beta.hat.logit, Sigma.logit)
+beta.tilde.logit <- mvrnorm(10000, beta.hat.logit, Sigma.logit)
 
 Sigma.logistf <- vcov(logistf)
-beta.tilde.logistf <- mvrnorm(1000, beta.hat.logistf, Sigma.logistf)
+beta.tilde.logistf <- mvrnorm(10000, beta.hat.logistf, Sigma.logistf)
 
 # OLS first differences
 p.tilde.lpm <- X.c%*%t(beta.tilde.lpm)
@@ -144,31 +142,104 @@ mean(fd.tilde.logistf < 0)
 mean(fd.tilde.logit < 0)
 mean(fd.tilde.b < 0)
 
-# these are extra checks, you don't need to run these
-
-# some checks on the Jeffrey's prior simulations
-# coda::effectiveSize(jeffreys$mcmc.chains)
-# coda::autocorr.plot(jeffreys$mcmc.chains)
-# coda::gelman.diag(jeffreys$mcmc.chains)
-# coda::gelman.plot(jeffreys$mcmc.chains)
-
-# compare jeffreys' logit highest posterior density with logistf profile likelihood
-# HPDinterval(as.mcmc(jeffreys$mcmc))
-# logistf
-
 # distribution plot of first differences
-plot(density(fd.tilde.logit), main = "Coordination First Differences", 
-     lwd = 2, xlab = "", ylim = c(0, 2.5))
-lines(quantile(fd.tilde.logit, 0.05), lty = 3)
-lines(quantile(fd.tilde.logit, 0.95), lty = 3)
-lines(density(fd.tilde.logistf), col = "grey", lwd = 3)
-lines(density(fd.tilde.b), col = "red", lwd = 2)
+plot(density(fd.tilde.logistf, n = 2000), main = "Simulated First Differences", 
+     lwd = 3, xlab = "", xlim = c(-0.2, 0.95))
+lines(density(fd.tilde.logit, n = 2000), col = "grey", lwd = 3)
+lines(density(fd.tilde.b, n = 2000), col = "red", lwd = 3)
 
 # some confidence intervals
 quantile(fd.tilde.lpm, c(0.05, 0.50, 0.95))
 quantile(fd.tilde.logit, c(0.05, 0.50, 0.95))
 quantile(fd.tilde.logistf, c(0.05, 0.50, 0.95))
 quantile(fd.tilde.b, c(0.05, 0.50, 0.95))
+
+# coefficient plot
+# rescale variables
+d$st_polity_conq <- rescale(d$polity_conq)
+d$st_lndist <- rescale(d$lndist)
+d$st_terrain_alt <- rescale(d$terrain_alt)
+d$st_soldperterr <- rescale(d$soldperterr)
+d$st_gdppc2_alt <- rescale(d$gdppc2_al)
+d$st_coord <- rescale(d$coord)
+
+# formula
+f <- resist ~ st_polity_conq + st_lndist + st_terrain_alt + 
+  st_soldperterr + st_gdppc2_alt + st_coord
+
+# estimate models
+lpm <- lm(f, d)
+logit <- glm(f, d, family = binomial)
+logistf <- logistf(f, d, alpha = 0.1)
+bootstrap <- boot(d, statistic = firth, R = 1000, f = f)
+
+varnames <- c("Constant",
+              "Polity",
+              "log(Distance)",
+              "Terrain",
+              "soldperterr",
+              "coord")
+
+par(mfrow = c(2,3), mar = c(1,1,1,1), oma = c(3,1,1,1))
+for (i in 2:length(varnames)) {
+  eplot(xlim = c(-8, 8), ylim = c(0, 5.5),
+        xlab = "Coefficient", 
+        anny = FALSE,
+        main = varnames[i])
+  if (i == 4) {
+    addxaxis()
+  }
+  polygon(c(-8, 8, 8, -8), c(4.1, 4.1, 4.9, 4.9) + .18,
+          border = NA, col = "grey80")
+  abline(v = 0, col = "grey50")
+  
+  
+  # PMLE w/ bootstrap
+  ht <- 0.5
+  est <- coef(logistf)[i]
+  ci.lo <- quantile(bootstrap$t[, i], 0.05)
+  ci.hi <- quantile(bootstrap$t[, i], 0.95)
+  points(est, ht, pch = 19)
+  lines(c(ci.lo, ci.hi), c(ht, ht))
+  text(est, ht, "PMLE w/ bootstrap", pos = 3, cex = 0.7)
+  # PMLE w/ lik. prof.
+  ht <- 1.5
+  est <- coef(logistf)[i]
+  ci.lo <- logistf$ci.lower[i]
+  ci.hi <- logistf$ci.upper[i]
+  points(est, ht, pch = 19)
+  lines(c(ci.lo, ci.hi), c(ht, ht))
+  text(est, ht, "PMLE w/ lik. prof.", pos = 3, cex = 0.7)
+  # PMLE w/ asymp. approx
+  ht <- 2.5
+  est <- coef(logistf)[i]
+  se <- sqrt(diag(vcov(logistf)))[i]
+  ci.lo <- est - 1.64*se
+  ci.hi <- est + 1.64*se
+  points(est, ht, pch = 19)
+  lines(c(ci.lo, ci.hi), c(ht, ht))
+  text(est, ht, "PMLE w/ asymp. approx.", pos = 3, cex = 0.7)
+  # MLE
+  ht <- 3.5
+  est <- coef(logit)[i]
+  se <- sqrt(diag(vcov(logit)))[i]
+  ci.lo <- est - 1.64*se
+  ci.hi <- est + 1.64*se
+  points(est, ht, pch = 19)
+  lines(c(ci.lo, ci.hi), c(ht, ht))
+  text(est, ht, "MLE", pos = 3, cex = 0.7)
+  # LS
+  ht <- 4.5
+  est <- coef(lpm)[i]
+  se <- sqrt(diag(vcov(lpm)))[i]
+  ci.lo <- est - 1.64*se
+  ci.hi <- est + 1.64*se
+  points(est, ht, pch = 19)
+  lines(c(ci.lo, ci.hi), c(ht, ht))
+  text(est, ht, "LPM", pos = 3, cex = 0.7)
+}
+
+
 
 
 
