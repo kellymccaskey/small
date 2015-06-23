28915f9eb0e48f32f9491ac36b67347f805b63cd
carlislerainey <carlislerainey@gmail.com>
2015-06-22 20:18:33 -0400

    add simulations to do-all.R

--- do-all.R
+++ do-all.R
@@ -1,4 +1,16 @@
-#
+# global parameters
+
+run_sims <- TRUE  # it takes about 5 hours to run these
+
+# ----------------------- #
+# monte carlo simulations #
+# ----------------------- #
+
+# do monte carlo simulations
+if (run_sims == TRUE) { source("R/simulations.R") }
+
+# plot simulation results
+source("R/plot-simulations.R")
 
 # -------------------- #
 # weisiger replciation #
