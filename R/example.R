ea1eec6c9bd4bb027406eabc3219d76b06da7939
carlislerainey <carlislerainey@gmail.com>
2015-07-08 13:37:29 -0500

    make example get data from web

--- R/example.R
+++ R/example.R
@@ -1,15 +1,15 @@
 
-# load data
+# load data from web
 library(readr)  # for read_csv()
-weisiger <- read_csv("weisiger-replication/data/weisiger.csv")
+weisiger <- read_csv("https://raw.githubusercontent.com/kellymccaskey/small/master/weisiger-replication/data/weisiger.csv") 
 
 # quick look at data
 library(dplyr)  # for glimpse()
 glimpse(weisiger)
 
 # model formula
-f <- resist ~ polity_conq + lndist + terrain_alt + 
-  soldperterr + gdppc2_alt + coord
+f <- resist ~ polity_conq + lndist + terrain + 
+  soldperterr + gdppc2 + coord
 
 # ----------------------------- #
 # pmle with the logistf package #
